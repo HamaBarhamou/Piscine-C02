e2e31c16ac29ce219af7ee1799c43572dd0a3ea4
bissaka- <bissaka-@student.s19.be>
2021-09-20 18:57:12 +0000

    Correction

--- ex06/ft_str_is_printable.c
+++ ex06/ft_str_is_printable.c
@@ -10,31 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	is_printable(char c)
-{
-	 if (c >= '!' && c <= '~')
-		return (1);
-	else
-		return (0);
-}
-
 int	ft_str_is_printable(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (is_printable(str[i]) == 1 && str[i])
+	while (str[i])
+	{
+		if ((str[i] < 32) || (str[i] > 126))
+			return (0);
 		i++;
-	if (str[0] == '\0' || str[0] == '\n')
-		return (1);
-	if (str[i] == '\0' || str[i] == '\n')
-		return (1);
-	else
-		return (0);
+	}
+	return (1);
 }
 
-/*int	main(void)
+int	main(void)
 {
 	char chaine[]=" ";
 	printf("%d\n",ft_str_is_printable(chaine));
-}*/
+	return (0);
+}
